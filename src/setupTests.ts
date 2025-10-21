c2bf74cbef4876aaf461c23b27ce3cf334339a18
NIKOLAY VIKTOROVICH <145424511+NikolayViktorovich@users.noreply.github.com>
2025-10-21 16:44:18 +0500

    upd
    upd

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,14 @@
 import '@testing-library/jest-dom';
 
 jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
   BrowserRouter: ({ children }: { children: React.ReactNode }) => children,
   Routes: ({ children }: { children: React.ReactNode }) => children,
   Route: ({ children }: { children: React.ReactNode }) => children,
   useNavigate: () => jest.fn(),
   useLocation: () => ({ pathname: '/' }),
+}));
+
+jest.mock('superjson', () => ({
+  stringify: (data: any) => JSON.stringify(data),
+  parse: (data: string) => JSON.parse(data),
 }));
